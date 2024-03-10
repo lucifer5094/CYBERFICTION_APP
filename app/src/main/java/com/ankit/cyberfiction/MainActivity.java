29769068ad0fbeed5461a085849b49619b237538
lucifer5094 <ankitraj85455@gmail.com>
2024-03-10 16:09:22 +0530

    Initial commit

---
+++ app/src/main/java/com/ankit/cyberfiction/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ankit.cyberfiction;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Setting up WebView
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true); // Enable JavaScript if required
+        webView.loadUrl("https://lucifer5094.github.io/CYBERFICTION/");
+
+        // Applying window insets
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
